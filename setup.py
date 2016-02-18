df725199803bb280cb0179107b7f6713de14a274
Tim Ryan <tim@3drobotics.com>
2016-02-18 14:55:44 -0800

    1.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dronekit-solo',
-      version='1.1.0',
+      version='1.2.0',
       description='DroneKit methods for Solo-specific features.',
       author='Tim Ryan',
       author_email='tim@3drobotics.com',
